e4ac2abd2c0833a8caa6e02acb372ecc4f85a9fe
Dharmin Nagar <dharminnagar@gmail.com>
2025-03-14 16:45:25 +0530

    feat: implement POST handler for user roast retrieval with error handling

--- app/[username]/route.ts
+++ app/[username]/route.ts
@@ -1,3 +1,50 @@
-// Call /user/[username] to fetch the user's Details + README
-// Call /getRoast/[username] to fetch the user's Roast from AI
-// Return the User Details + Roast in the response
+/**
+ * A handler for the POST request to get a roast of the user
+ * @param req roastLength: number, level: "mild" | "medium" | "extreme"
+ * @param param1 username: string
+ * @returns A roast of the user based on their profile data
+ */
+async function handler(
+    req: Request,
+    { params }: { params: { username: string } }
+) {
+    const SERVER_URL = process.env.SERVER_URL;
+    if (!SERVER_URL) {
+        return new Response(JSON.stringify({ error: "Server URL not found" }));
+    }
+
+    const { username } = await params;
+    const { roastLength, level = "mild" } = await req.json();
+    try {
+        const userDetails = await (
+            await fetch(`${SERVER_URL}/user/${username}`)
+        ).text();
+
+        if (!userDetails) {
+            return new Response(
+                JSON.stringify({ error: "User Details not found" })
+            );
+        }
+
+        const roast = await (
+            await fetch(`${SERVER_URL}/getRoast/${username}`, {
+                method: "POST",
+                body: JSON.stringify({
+                    userDetails,
+                    roastLength: roastLength,
+                    level: level,
+                }),
+            })
+        ).text();
+
+        return new Response(JSON.stringify(roast), {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+    } catch (error: any) {
+        return new Response(JSON.stringify({ error: error.message }));
+    }
+}
+
+export { handler as POST };
